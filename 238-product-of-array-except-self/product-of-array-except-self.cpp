9aa94c8187ff7fba6e0e440eff51a33350fecd4a
Faareh Ahmed <122796902+Faareh-Ahmed@users.noreply.github.com>
2024-03-11 19:32:16 +0500

    Time: 16 ms (60.68%) | Memory: 27.2 MB (39.98%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,9 +4,13 @@ public:
 
         int product = 1;
         int n = nums.size();
+        // Count the number of zeroes in the array
         int zerocount = 0;
+        // vector to store the results
         vector<int> newproduct;
 
+        // Loop that counts the Number of Zeroes and also finds the Total product of all non zero
+        // elements in the array
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
                 zerocount++;
@@ -18,17 +22,21 @@ public:
 
 
         for (int i = 0; i < n; i++) {
-
+            
+            // If there are more than 1 zero elements then simply push 0 in resultant array
             if(zerocount>1)
             {
                 newproduct.push_back(0);
             }
+
+            // For non zero element, push back the product/element
             else if (nums[i] != 0 && zerocount < 1) {
                 newproduct.push_back(product / nums[i]);
             }
             else if (nums[i] != 0 && zerocount >= 1) {
                 newproduct.push_back(0);
             }
+            // If the element is the only single Zero element then push the Product found
             else if (nums[i] == 0 && zerocount == 1) {
                 newproduct.push_back(product);
             }
