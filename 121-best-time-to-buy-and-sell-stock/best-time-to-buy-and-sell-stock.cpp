530147bf157b5863de4523d42cad34e2d4a05155
Faareh Ahmed <122796902+Faareh-Ahmed@users.noreply.github.com>
2024-03-12 22:54:54 +0500

    Time: 97 ms (21.82%) | Memory: 95.9 MB (28.26%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,38 +2,31 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        // int minimum=prices[0];
         int profit=0;
-        int current;
         int n=prices.size();
 
-        // for(int i=0;i<n;i++)
-        // {
-        //     current=prices[i];
-        //     if(current-minimum>profit)
-        //     {
-        //         profit=current-minimum;
-        //     }
-        //     if(current<minimum)
-        //     {
-        //         minimum=current;
-        //     }
-        // }
-
+        //start pointers: i = Buy Pointer, j = Sell Pointer
         int i=0;
         int j=i+1;
 
         while(j<n)
         {
+            // If the current difference bwtween Sell and Buy is > Profit, then Update Profit
             if(prices[j]-prices[i]>profit)
             {
                 profit=prices[j]-prices[i];
             }
+
+            // If the Sell price is Greater than Buy price, then move the Sell pointer 1 forward
+            // Else Means that Buy price is Greater than Sell Price, So move the Buy pointer 1 forward
             if(prices[j]>prices[i])
             {
                 j++;
             }else{
                 i++;
+
+                // Check if the Buy and Sell Pointer at same index then move the Sell pointer also
+                // Because we must sell at a day greater than Buy day
                 if(i==j)
                 {
                     j++;
