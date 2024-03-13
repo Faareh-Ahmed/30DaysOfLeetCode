053efd35aa3febf922e0da61920dd1d8486937ee
Faareh Ahmed <122796902+Faareh-Ahmed@users.noreply.github.com>
2024-03-13 14:24:56 +0500

    Time: 11 ms (80.87%) | Memory: 20.4 MB (57.69%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,60 +5,6 @@ public:
         int n=nums.size();
 
 
-        // By HASHING :
-
-        // bool *findnumber=new bool[n+1];
-        // bool found=false;
-
-        // for (int i = 0; i < n; ++i)
-        // {
-        //     findnumber[i] = false;
-        // }
-
-        // for(int i=0;i<n;i++)
-        // {
-        //     findnumber[nums[i]]=true;
-        // }
-
-        // for (int i = 0; i < n; ++i)
-        // {
-        //     if(findnumber[i]==false)
-        //     {
-        //         found=true;
-        //         return i;
-        //     }
-        // }
-
-        // if(found==false)
-        // {
-        //     return n;
-        // }
-
-        // return n;
-
-
-        // By BITWISE MANIPULATION:
-
-
-        // int arrayxor=nums[0];
-
-        // for(int i=1;i<n;i++)
-        // {
-        //     arrayxor^=nums[i];
-        // }
-
-        // cout<<"arrayxor: "<<arrayxor<<endl;
-
-        // int naturalxor=0;
-
-        // for(int i=1;i<=n;i++)
-        // {
-        //     naturalxor^=i;
-        // }
-
-        // cout<<"naturalxor: "<<naturalxor<<endl;
-                // return naturalxor^arrayxor;
-
 
         // By SUM OF N NATURAL NUMBERS:
 
@@ -70,10 +16,7 @@ public:
             sumofarray+=nums[i];
         }
 
-        for(int i=1;i<=n;i++)
-        {
-            sumofnatural+=i;
-        }
+        sumofnatural=n*(n+1)/2;
         
         return sumofnatural-sumofarray;
 
