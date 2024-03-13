0d5581a5cf7b0a332936af5ec62ec4d4ede49427
Faareh Ahmed <122796902+Faareh-Ahmed@users.noreply.github.com>
2024-03-13 14:09:18 +0500

    Time: 16 ms (38.64%) | Memory: 20.2 MB (82.86%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,36 +3,55 @@ public:
     int missingNumber(vector<int>& nums) {
         
         int n=nums.size();
-        bool *findnumber=new bool[n+1];
-        bool found=false;
+        // bool *findnumber=new bool[n+1];
+        // bool found=false;
 
-        for (int i = 0; i < n; ++i)
+        // for (int i = 0; i < n; ++i)
+        // {
+        //     findnumber[i] = false;
+        // }
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     findnumber[nums[i]]=true;
+        // }
+
+        // for (int i = 0; i < n; ++i)
+        // {
+        //     if(findnumber[i]==false)
+        //     {
+        //         found=true;
+        //         return i;
+        //     }
+        // }
+
+        // if(found==false)
+        // {
+        //     return n;
+        // }
+
+        // return n;
+
+        int arrayxor=nums[0];
+
+        for(int i=1;i<n;i++)
         {
-            findnumber[i] = false;
+            arrayxor^=nums[i];
         }
 
-        for(int i=0;i<n;i++)
+        cout<<"arrayxor: "<<arrayxor<<endl;
+
+        int naturalxor=0;
+
+        for(int i=1;i<=n;i++)
         {
-            findnumber[nums[i]]=true;
+            naturalxor^=i;
         }
 
-        for (int i = 0; i < n; ++i)
-        {
-            if(findnumber[i]==false)
-            {
-                found=true;
-                return i;
-            }
-        }
-
-        if(found==false)
-        {
-            return n;
-        }
-
-        return n;
-
-
+        cout<<"naturalxor: "<<naturalxor<<endl;
+        
+        
+        return naturalxor^arrayxor;
 
     }
 };
